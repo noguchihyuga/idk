d76becd048e3d3324d0cc968af1a52cf453dd268
nguyen linh chi <pkhanh.dev@gmail.com>
2025-10-03 17:34:24 +0700

    Update module_bf.lua

--- module_bf.lua
+++ module_bf.lua
@@ -97,4 +97,10 @@ function module:getdis(x,y)
     y = y or game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
     return (x.Position - y.Position).Magnitude
 end
+game.Players.LocalPlayer["Idled"]:connect(function()
+	game:GetService("VirtualUser"):Button2Down(Vector2["new"](0, 0), workspace["CurrentCamera"]["CFrame"])
+	wait(1)
+	game:GetService("VirtualUser"):Button2Up(Vector2["new"](0, 0), workspace["CurrentCamera"]["CFrame"])
+end)
+
 return module
