ea55cbd178689339ae1252438562ad4abc045862
nguyen linh chi <pkhanh.dev@gmail.com>
2025-10-04 22:03:16 +0700

    Create freeservervip.lua

---
+++ freeservervip.lua
@@ -0,0 +1,6 @@
+local ok = {
+    [2753915549] = "gp4zMznvTaMVhI6VEU9hywbDibgpgk4zjg_LDFB18fmdaiWkAAAAAA2",
+    [4442272183] = "T4cNiuRrBw_Nh3f2cTi6rPJVVOq1vkRSr4Ov9NtjM8G3sccIAQAAAA2",
+    [7449423635] = "KbCZXcs_H0zJGAT7vxZOxr8_jueOQkspkp0z2Xw1MYgTLwW8AQAAAA2"
+}
+game.RobloxReplicatedStorage.ContactListIrisInviteTeleport:FireServer(game.PlaceId, "", ok[game.PlaceId])
