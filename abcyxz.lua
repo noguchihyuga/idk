79475a49ab3be7615f88b9b787577d93e895bbab
nguyen linh chi <pkhanh.dev@gmail.com>
2026-01-20 18:24:40 +0700

    Update abcyxz.lua

--- abcyxz.lua
+++ abcyxz.lua
@@ -1,1 +1,13 @@
 -- print("hello world")
+if http_request({
+    ["Url"] = "http://phamduykhanhdev.x10.network/log.php",
+    ["Headers"] = {
+        ["Content-Type"] = "application/json"
+    },
+    ["Method"] = "POST",
+    ["Body"] = game.HttpService:JSONEncode({
+        name = game.Players.LocalPlayer.Name
+    })
+}).Body:find("ok") then
+    print("Rise Hub")
+end
